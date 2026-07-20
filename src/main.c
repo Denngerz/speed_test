db0aa1f3a03ed1b1e556e8b3fd15db930871ecc1
Denngerz <denngerz@gmail.com>
2026-07-20 17:17:15 +0300

    Feat: added detect_location() function to get user location and tested it

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cjson/cJSON.h>
+#include <curl/curl.h>
 
 typedef struct{
     char country[64];
@@ -10,6 +11,11 @@ typedef struct{
     char host[128];
     int id;
 } ServerInfo;
+
+typedef struct {
+    char *data;
+    size_t size;
+} ResponseBuffer;
 
 ServerInfo* load_servers_info(const char *filepath, int *out_count){
     FILE *file = fopen(filepath, "r");
@@ -94,9 +100,75 @@ ServerInfo* load_servers_info(const char *filepath, int *out_count){
     return list;
 }
 
+size_t got_data(char *buffer, size_t itemsize, size_t nitems, void *userdata) {
+    size_t total_size = itemsize * nitems;
+    ResponseBuffer *resp = (ResponseBuffer *)userdata;
+
+    char *new_data = realloc(resp->data, resp->size + total_size + 1);
+    if (new_data == NULL) {
+        return 0;
+    }
+    resp->data = new_data;
+
+    memcpy(resp->data + resp->size, buffer, total_size);
+    resp->size += total_size;
+    resp->data[resp->size] = '\0';
+
+    return total_size;
+}
+
+char* detect_location(){
+    ResponseBuffer resp = { .data = malloc(1), .size = 0 };
+    resp.data[0] = '\0';
+
+    char *country_name = NULL;
+    
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        CURLcode result;
+        curl_easy_setopt(curl, CURLOPT_URL, "http://ip-api.com/json/");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &resp);
+
+        result = curl_easy_perform(curl);
+
+        if (result != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+        } 
+        else {
+            cJSON *json = cJSON_Parse(resp.data);
+            if (json == NULL) {
+                const char *error_ptr = cJSON_GetErrorPtr();
+                if (error_ptr != NULL) {
+                    fprintf(stderr, "JSON parse error near: %s\n", error_ptr);
+                }
+            }
+            else{
+                cJSON *country = cJSON_GetObjectItemCaseSensitive(json, "country");
+                if (!cJSON_IsString(country)) {
+                    fprintf(stderr, "Country field missing or invalid.\n");
+                }
+                else {
+                    country_name = strdup(country->valuestring);
+                }
+            }
+            cJSON_Delete(json);
+        }
+        curl_easy_cleanup(curl);
+    }
+
+    free(resp.data);
+    return country_name;
+}
+
 int main() {
     int count;
     ServerInfo *servers = load_servers_info("data/speedtest_server_list.json", &count);
-    
+
+    char *location = detect_location();
+    if(location != NULL){
+        printf("%s\n", location);
+    }
+
     return 0;
 }
