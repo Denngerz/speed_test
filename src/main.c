d3b493429a652cb6a8a15400f078232c03db36ea
Denngerz <denngerz@gmail.com>
2026-07-22 16:36:08 +0300

    Feat: added getopt to main.c, enabling individual download, upload, location, and full test options

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <cjson/cJSON.h>
 #include <curl/curl.h>
+#include <unistd.h>
 
 typedef struct {
     char country[64];
@@ -291,30 +292,114 @@ ServerInfo* find_server_by_country(ServerInfo *servers, int count, const char *c
     return NULL;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     int count;
     ServerInfo *all_servers = load_servers_info("data/speedtest_server_list.json", &count);
 
-    char *location = detect_location();
-    if(location != NULL){
+    int run_download = 0, run_upload = 0, run_location = 0, run_all = 0;
+    char *server_host = NULL;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "ludas:")) != -1) {
+        switch (opt) {
+            case 'l': {
+                run_location = 1; 
+                break;
+            }
+            case 'u':{
+                run_upload = 1;
+                break;
+            }
+            case 'd': {
+                run_download = 1;
+                break;
+            }
+            case 'a': {
+                run_all = 1;
+                break;
+            }
+            case 's': {
+                server_host = optarg;
+                break;
+            }
+            default:
+                return 1;
+        }
+    }
+
+    if(run_location == 1){
+        printf("\n===Location===\n");
+
+        char *location = detect_location();
+
+        if(location == NULL){
+            printf("Coundn`t detect user location\n");
+        }
+        else{
+            printf("Your location: %s\n", location);
+        }
+
+        free(location);
+
+        printf("================\n");
+    }
+    if(run_upload == 1){
+        printf("\n===Upload Speed===\n");
+
+        if(server_host == NULL){
+            printf("Server host is not provided (-s)\n");
+        }
+        else{
+            double random_server_upload_speed = get_server_upload_speed(server_host);
+            printf("Data upload speed: %f Mb\n", random_server_upload_speed);
+        }
+
+        printf("================\n");
+    }
+    if(run_download == 1){
+        printf("\n===Download Speed===\n");
+
+        if(server_host == NULL){
+            printf("Server host is not provided (-s)\n");
+        }
+        else{
+            double random_server_download_speed = get_server_download_speed(server_host);
+            printf("Data download speed: %f Mb\n", random_server_download_speed);
+        }
+
+        printf("================\n");
+    }
+    if(run_all == 1){
+        printf("\n===Full test===\n");
+
+        char *location = detect_location();
+
         printf("Your location: %s\n", location);
+
+        if(location == NULL){
+            printf("Coundn`t detect user location");
+        }
+        else{
+            ServerInfo *matching_server = find_server_by_country(all_servers, count, location);
+
+            if(matching_server == NULL){
+                printf("Coudn`t find a valid server\n");
+                return 1;
+            }
+
+            printf("Server with which test is being performed: %s\n", matching_server->host);
+            
+            double random_server_download_speed = get_server_download_speed(matching_server->host);
+            printf("Data download speed: %f Mb\n", random_server_download_speed);
+
+            double random_server_upload_speed = get_server_upload_speed(matching_server->host);
+            printf("Data upload speed: %f Mb\n", random_server_upload_speed);
+            
+            free(location);
+        }
+
+         printf("================\n");
     }
-
-    ServerInfo *matching_server = find_server_by_country(all_servers, count, location);
-
-    if(matching_server == NULL){
-        return 1;
-    }
-
-    printf("Chosen server: %s\n", matching_server->host);
-
-    double random_server_download_speed = get_server_download_speed(matching_server->host);
- 
-    printf("Download speed: %f Mb\n", random_server_download_speed);
-    
-    double random_server_upload_speed = get_server_upload_speed(matching_server->host);
-
-    printf("Upload speed: %f Mb\n", random_server_upload_speed);
 
     free(all_servers);
     return 0;
